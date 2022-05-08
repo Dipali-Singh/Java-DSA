aac9e36dbd623a548683966cd3b51527d1b0758a
Dipali-Singh <66013709+Dipali-Singh@users.noreply.github.com>
2022-05-08 14:27:13 +0530

    area

--- Java-DSA/Day-1/Area_Rectangle.java
+++ Java-DSA/Day-1/Area_Rectangle.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-class Sum_Integer {
+class Area_Rectangle {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n1 = sc.nextInt();
